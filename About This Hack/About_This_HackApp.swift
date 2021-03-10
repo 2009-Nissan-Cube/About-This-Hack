4bd95af1e04a9b6d53a9172be145bea42f157564
AvaQueen <avaq970gmail.com>
2021-03-10 15:21:10 -0500

    Initial Commit

---
+++ About This Hack/About_This_HackApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_This_HackApp.swift
+//  About This Hack
+//
+//  Created by Alexander Skula on 3/10/21.
+//
+
+import SwiftUI
+
+@main
+struct About_This_HackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ About This Hack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  About This Hack
+//
+//  Created by Alexander Skula on 3/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
