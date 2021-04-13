836cd76d3e7577a81da9f8a9889b017e680b9a0e
AvaQueen <avaq970gmail.com>
2021-04-13 17:44:37 -0400

    Hi, does this work?

---
+++ About This Hack/modelID.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl -s 'https://support-sp.apple.com/sp/product?cc='$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}' | cut -c 9-)  | sed 's|.*<configCode>\(.*\)</configCode>.*|\1|'
+
+
+
+
+
