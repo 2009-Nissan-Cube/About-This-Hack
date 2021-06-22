eebfa62f14b78774bd308f7d6fd3b944c4fb9e95
AvaQueen <avaq970gmail.com>
2021-06-22 11:56:38 -0400

    Fix close on quit

--- About This Hack/About_This_HackApp.swift
+++ About This Hack/About_This_HackApp.swift
@@ -9,9 +9,19 @@ import SwiftUI
 
 @main
 struct About_This_HackApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
+            .onReceive(NotificationCenter.default.publisher(for: NSApplication.willUpdateNotification), perform: { _ in
+                hideButtons()
+            })
+        }
+    }
+    func hideButtons() {
+        for window in NSApplication.shared.windows {
+            window.standardWindowButton(NSWindow.ButtonType.zoomButton)!.isHidden = true
+            //window.standardWindowButton(NSWindow.ButtonType.miniaturizeButton)!.isHidden = true
         }
     }
 }
