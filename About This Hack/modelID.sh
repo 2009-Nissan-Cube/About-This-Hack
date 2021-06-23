2b129b08c93854287f396dd6edddef2fd4f65781
AvaQueen <avaq970gmail.com>
2021-06-23 10:31:24 -0400

    Delete modelID.sh, no longer needed

--- About This Hack/modelID.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -s 'https://support-sp.apple.com/sp/product?cc='$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}' | cut -c 9-)  | sed 's|.*<configCode>\(.*\)</configCode>.*|\1|'
