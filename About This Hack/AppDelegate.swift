6e89228c9886344fdb682cfe302e6d02b865115d
8-Bit Cat <79278890+8itCat@users.noreply.github.com>
2021-08-20 08:23:56 -0400

    Initial Commit

---
+++ About This Hack/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  About This Hack
+//
+//  Created by Alexander Skula on 8/20/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ About This Hack/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  About This Hack
+//
+//  Created by Alexander Skula on 8/20/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
