ade20353c3751f5310a846a1e2c4faf43429e6db
8-Bit Cat <79278890+8itCat@users.noreply.github.com>
2021-08-24 12:58:19 -0400

    Oh boy is this better

--- About This Hack/AppDelegate.swift
+++ About This Hack/AppDelegate.swift
@@ -24,6 +24,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
 
 }
 
