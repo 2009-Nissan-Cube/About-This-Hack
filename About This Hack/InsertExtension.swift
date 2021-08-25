32b88153a28152482a9e1b20ea48b2576ca73b6f
8-Bit Cat <79278890+8itCat@users.noreply.github.com>
2021-08-25 19:05:17 -0400

    I would appreciate if this actually worked already...

---
+++ About This Hack/InsertExtension.swift
@@ -0,0 +1,22 @@
+//
+//  InsertExtension.swift
+//  InsertExtension
+//
+//  Created by 8itCat on 8/24/21.
+//
+
+import Foundation
+
+extension String {
+    func inserting(separator: String, every n: Int) -> String {
+        var result: String = ""
+        let characters = Array(self)
+        stride(from: 0, to: characters.count, by: n).forEach {
+            result += String(characters[$0..<min($0+n, characters.count)])
+            if $0+n < characters.count {
+                result += separator
+            }
+        }
+        return result
+    }
+}
