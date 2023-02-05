dc0212d33105455dc04ac52d24b0d5bbeb96c76d
0xCUBE <94565160+0xCUB3@users.noreply.github.com>
2023-02-05 13:36:26 -0500

    Switch Shell Commands to Zsh

--- About This Hack/Shell.swift
+++ About This Hack/Shell.swift
@@ -7,13 +7,18 @@
 import Foundation
 
 // Allows native runnning of Terminal commands
-func run(_ cmd: String) -> String {
+func run(_ command: String) -> String {
+    let task = Process()
     let pipe = Pipe()
-    let process = Process()
-    process.launchPath = "/bin/sh"
-    process.arguments = ["-c", String(format:"%@", cmd)]
-    process.standardOutput = pipe
-    let fileHandle = pipe.fileHandleForReading
-    process.launch()
-    return String(data: fileHandle.readDataToEndOfFile(), encoding: .utf8) ?? "Oops"
+
+    task.standardOutput = pipe
+    task.arguments = ["-c", command]
+    task.launchPath = "/bin/zsh"
+    task.launch()
+
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output = String(data: data, encoding: .utf8)!
+
+    return output
+    
 }
