1b661b7db7694750044d4f6544bb7faeda473771
nocontent06 <spasst.spiel@gmail.com>
2023-07-19 17:05:36 +0200

    First Version of Dual GPU Support!
    Currently no better solution... since we do not have enough space to put the GPUs under each other. :(

--- About This Hack/HardwareCollectors/HCGPU.swift
+++ About This Hack/HardwareCollectors/HCGPU.swift
@@ -11,17 +11,31 @@ class HCGPU {
     
     
     static func getGPU() -> String {
-        let graphicsTmp = run("cat ~/.ath/scr.txt | grep 'Chipset' | sed 's/.*: //'")
-        let graphicsRAM  = run("cat ~/.ath/scr.txt | grep VRAM | sed 's/.*: //'")
-        let graphicsArray = graphicsTmp.components(separatedBy: "\n")
-        let vramArray = graphicsRAM.components(separatedBy: "\n")
-        _ = graphicsArray.count
-        var x = 0
-        var gpuInfoFormatted = ""
-        while x < min(vramArray.count, graphicsArray.count) {
-            gpuInfoFormatted.append("\(graphicsArray[x]) \(vramArray[x])\n")
-            x += 1
+        var graphicsTmp = run("cat ~/.ath/scr.txt | grep 'Chipset' | sed 's/.*: //'")
+        if graphicsTmp.contains("Intel") || graphicsTmp.contains("NVIDIA") {
+            graphicsTmp = graphicsTmp.replacingOccurrences(of: "Intel ", with: "")
+            graphicsTmp = graphicsTmp.replacingOccurrences(of: "NVIDIA ", with: "")
         }
+        var graphicsRAM  = run("cat ~/.ath/scr.txt | grep VRAM | sed 's/.*: //'")
+        let graphicsArray = graphicsTmp.components(separatedBy: "\n")
+        print(graphicsArray.count)
+        let vramArray = graphicsRAM.components(separatedBy: "\n")
+        var gpuInfoFormatted = ""
+        if graphicsArray.count == 1 {
+            gpuInfoFormatted = "\(graphicsArray[0]) \(vramArray[0])"
+        } else {
+            for index in 0..<graphicsArray.count {
+                gpuInfoFormatted += "\(graphicsArray[index]) \(vramArray[index])"
+                if index <= graphicsArray.count - 3 {
+                    gpuInfoFormatted += " + "
+                }
+            }
+        }
+
+//        while x < min(vramArray.count, graphicsArray.count) {
+//            gpuInfoFormatted.append("\(graphicsArray[x]) \(vramArray[x])\n")
+//            x += 1
+//        }
         return gpuInfoFormatted
     }
     
