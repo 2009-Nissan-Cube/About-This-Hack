d223389e9f833953fe68160b89a282e0ea30a0e2
0xCUBE <94565160+0xCUB3@users.noreply.github.com>
2023-09-03 12:08:29 -0400

    Fix RAM Issues

--- About This Hack/HardwareCollectors/HCRAM.swift
+++ About This Hack/HardwareCollectors/HCRAM.swift
@@ -1,12 +1,3 @@
-//
-//  HCRAM.swift
-//  About This Hack
-//
-//  Created by Felix on 15.07.23.
-//
-
-import Cocoa
-
 class HCRAM {
     
     static func getRam() -> String {
@@ -15,7 +6,20 @@ class HCRAM {
         print("RAM Type: " + ramType)
         let ramSpeed = run("cat ~/.ath/sysmem.txt | grep 'Speed' | grep 'MHz' | awk '{print $2\" \"$3}' | sed -n '1p'").trimmingCharacters(in: .whitespacesAndNewlines)
         print("RAM Speed: " + ramSpeed)
-        let ramReturn = "\(ram) GB \(ramSpeed) \(ramType)"
-        return ramReturn
+        // If RAM type doesn't show up
+        if !ramType.contains("D") {
+            if !ramSpeed.contains("mpty") || ramSpeed != "" {
+                return "\(ram) GB \(ramSpeed)"
+            // If RAM speed doesn't show up either
+            } else {
+                return "\(ram) GB"
+            }
+        // If just RAM speed doesn't show up
+        } else if ramSpeed.contains("mpty") || ramSpeed == "" {
+            return "\(ram) GB \(ramType)"
+        // Normal case
+        } else {
+            return "\(ram) GB \(ramSpeed) \(ramType)"
+        }
     }
 }
