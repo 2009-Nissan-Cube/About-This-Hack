91911cff5c90f7b78074dc96fa5f82b203310e48
matxpa <70573409+matxpa@users.noreply.github.com>
2023-09-13 16:57:30 +0200

    Update HCStartupDisk.swift minor modif

--- About This Hack/HardwareCollectors/HCStartupDisk.swift
+++ About This Hack/HardwareCollectors/HCStartupDisk.swift
@@ -2,6 +2,6 @@ import Cocoa
 
 class HCStartupDisk {
     static func getStartupDisk() -> String {
-        return run("grep 'Volume Name' ~/.ath/sysvolname.txt | sed 's/.*:[[:space:]]*//' | tr -d '\n'")
+        return run("grep \"Volume Name\" ~/.ath/sysvolname.txt | sed 's/.*:[[:space:]]*//' | tr -d '\n'")
     }
 }
