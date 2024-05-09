2b7c9915fbabaf3459ddb7869b9555aa715722de
matxpa <70573409+matxpa@users.noreply.github.com>
2024-05-09 12:51:27 +0200

    Update HardwareCollector.swift
    Extension string removed

--- About This Hack/HardwareCollector.swift
+++ About This Hack/HardwareCollector.swift
@@ -156,31 +156,3 @@ class HardwareCollector {
         """, String(1 - percent)]
     }
 }
-
-
-extension String {
-
-    var length: Int {
-        return count
-    }
-
-    subscript (i: Int) -> String {
-        return self[i ..< i + 1]
-    }
-
-    func substring(fromIndex: Int) -> String {
-        return self[min(fromIndex, length) ..< length]
-    }
-
-    func substring(toIndex: Int) -> String {
-        return self[0 ..< max(0, toIndex)]
-    }
-
-    subscript (r: Range<Int>) -> String {
-        let range = Range(uncheckedBounds: (lower: max(0, min(length, r.lowerBound)),
-                                            upper: min(length, max(0, r.upperBound))))
-        let start = index(startIndex, offsetBy: range.lowerBound)
-        let end = index(start, offsetBy: range.upperBound - range.lowerBound)
-        return String(self[start ..< end])
-    }
-}
